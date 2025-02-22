0e1c3dd514c84a7d3d71f852a8150ee1def9f094
kemparaju <kemparaju.kemparaju@ibm.com>
2025-02-22 02:31:27 -0600

    fix: no attribute _ef_search

--- entrypoint_mamba_env.sh
+++ entrypoint_mamba_env.sh
@@ -11,4 +11,4 @@ python --version | grep 'Python 3.10'
 python -m pip install docker mariadb
 micromamba install -y -c conda-forge ansicolors==1.1.8 h5py==3.8.0 matplotlib==3.6.3 numpy==1.24.2 psutil==5.9.4 pytest==7.2.2
 micromamba install -y pyyaml==6.0 scikit-learn==1.2.1 jinja2==3.1.2 datasets==2.12.0
-python -u run_algorithm.py --dataset glove-100-angular --algorithm mariadb --runs 1 --count 5 --module ann_benchmarks.algorithms.mariadb --constructor MariaDB '["angular", {"txt": "test", "M": 8, "engine": "MyISAM"}]'
+python -u run_algorithm.py --dataset glove-100-angular --algorithm mariadb --runs 1 --count 5 --module ann_benchmarks.algorithms.mariadb --constructor MariaDB '["angular", {"txt": "test", "M": 8, "engine": "MyISAM"}]' '[[10], [20], [30], [40]]'
